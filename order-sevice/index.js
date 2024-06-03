7735e148a2b397a1edaabaa5fd0298c35c69ca88
arensin8 <arensinani4@gmail.com>
2024-06-03 11:47:49 +0400

    final fixings

--- order-sevice/index.js
+++ order-sevice/index.js
@@ -1,22 +1,3 @@
-const express = require("express");
-const { OrderRouter } = require("./handler/order");
 const { createOrderWithQueue } = require("./config/rabbitmq");
-const app = express();
-require("dotenv").config();
 require("./config/mongoose.config");
-
-const { PORT } = process.env;
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 createOrderWithQueue("ORDER");
-app.use("/order", OrderRouter);
-app.use((req, res, next) => {
-  return res.json({ error: "notfound" });
-});
-app.use((error, req, res, next) => {
-  return res.json({ error: error.message });
-});
-
-app.listen(PORT, () => {
-  console.log("Order-service running over port:", PORT);
-});
